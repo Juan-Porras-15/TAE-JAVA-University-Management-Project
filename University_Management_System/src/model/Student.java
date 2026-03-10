f51c35ad7845875854f6452138b19292195fc632
Juan Andrés Porras Vélez <j.porras@globant.com>
2026-03-10 11:46:53 -0500

    Created Student class

---
+++ University_Management_System/src/model/Student.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id;
+    }
+}
