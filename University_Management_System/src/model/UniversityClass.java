1bb1c71afdc5ca39b5dead617702d4a9bf6058a5
Juan Andrés Porras Vélez <j.porras@globant.com>
2026-03-10 12:08:27 -0500

    create_university_class_class

---
+++ University_Management_System/src/model/UniversityClass.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniversityClass {
+    private String name;
+    private String classroom;
+    private List<Student> studentList;
+    private Teacher teacher;
+
+    public UniversityClass(String name, String classroom, Teacher teacher) {
+        this.name = name;
+        this.classroom = classroom;
+        this.teacher = teacher;
+        this.studentList = new ArrayList<>();
+    }
+
+    public boolean addStudentToClass(Student student) {
+        for (Student s : studentList) {
+            if (s.getId() == student.getId()) {
+                return false;
+            }
+        }
+        this.studentList.add(student);
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getClassroom() {
+        return classroom;
+    }
+    public Teacher getTeacher() {
+        return teacher;
+    }
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+}
