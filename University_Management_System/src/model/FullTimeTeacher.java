712a3c9a44e2c87a844c2deb5463c8b0b198bd7a
Juan Andrés Porras Vélez <j.porras@globant.com>
2026-03-10 10:59:48 -0500

    Added PartTimeTeacher and FullTimeTeacher Classes

---
+++ University_Management_System/src/model/FullTimeTeacher.java
@@ -0,0 +1,19 @@
+package model;
+
+public class FullTimeTeacher extends Teacher {
+    private int experienceYears;
+
+    public FullTimeTeacher(String name, double baseSalary, int experienceYears) {
+        super(name, baseSalary);
+        this.experienceYears = experienceYears;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return getBaseSalary() * (1.1 * experienceYears);
+    }
+
+    public int getExperienceYears() {
+        return experienceYears;
+    }
+}
